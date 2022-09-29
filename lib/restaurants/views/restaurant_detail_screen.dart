d23a5112cc6e9af832fd961951d5b69ed2bdea85
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-29 10:54:54 +0900

    add Sliver List

--- lib/restaurants/views/restaurant_detail_screen.dart
+++ lib/restaurants/views/restaurant_detail_screen.dart
@@ -11,23 +11,77 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset(name),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '맛있음', '차즈'],
-            ratingsCount: 100,
-            deliveryTime: 30,
-            deliveryFee: 3600,
-            ratings: 4.76,
-            isDetail: true,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          )
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+      // child: Column(
+      //   children: [
+      //     RestaurantCard(
+      //       image: Image.asset(name),
+      //       name: '불타는 떡볶이',
+      //       tags: ['떡볶이', '맛있음', '차즈'],
+      //       ratingsCount: 100,
+      //       deliveryTime: 30,
+      //       deliveryFee: 3600,
+      //       ratings: 4.76,
+      //       isDetail: true,
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       child: ProductCard(),
+      //     )
+      //   ],
+      // ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('hi'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '맛있음', '차즈'],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3600,
+        ratings: 4.76,
+        isDetail: true,
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          'Menu',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
       ),
     );
   }
