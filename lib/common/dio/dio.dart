f21d1aa8c4b36b02f41ebeb445c9604711b26842
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-29 23:37:31 +0900

    add dio error interceptor

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -34,4 +34,47 @@ class CustomInterceptor extends Interceptor {
 
     return super.onRequest(options, handler);
   }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+
+   final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+   if (refreshToken == null) {
+     return handler.reject(err);
+   }
+
+   final isStatus401 = err.response?.statusCode == 401;
+   final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+   if (isStatus401 && !isPathRefresh) {
+     final dio = Dio();
+
+     try {
+       final resp = await dio.post('http://$ip/auth/token', options: Options(
+         headers: {
+           'authorization': 'Bearer $refreshToken'
+         },
+       ),);
+
+       final accessToken = resp.data['accessToken'];
+
+       final options = err.requestOptions;
+
+       options.headers.addAll({
+         'authorization': 'Bearer $accessToken'
+       });
+
+       await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+       final response = await dio.fetch(options);
+
+       return handler.resolve(response);
+     } on DioError catch(e) {
+       return handler.reject(err);
+     }
+   }
+
+    return handler.reject(err);
+  }
 }
